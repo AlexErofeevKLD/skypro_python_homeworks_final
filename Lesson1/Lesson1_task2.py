1c03b00a3e42b2bb1ea265d7f596bc60e3d953d9
AlexErofeevKLD <166793943+AlexErofeevKLD@users.noreply.github.com>
2024-05-17 16:29:15 +0300

    add first homework

---
+++ Lesson1/Lesson1_task2.py
@@ -0,0 +1,3 @@
+my_age = 37
+my_age = 41
+print(my_age)
---
+++ Lesson1/Lesson1_task4.py
@@ -0,0 +1,9 @@
+first_name = input("Ваше имя? ")
+print(first_name)
+
+last_name = input("Ваша фамилия? ")
+print(last_name)
+
+first_name = "Алексей"
+last_name = "Ерофеев"
+print("Вас зовут:", last_name, first_name)
