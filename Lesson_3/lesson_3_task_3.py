40ccf3ccb32b777e14f12c663e5b8666368a3b28
AlexErofeevKLD <166793943+AlexErofeevKLD@users.noreply.github.com>
2024-05-27 20:00:17 +0300

    add third homework

--- Lesson_3/lesson_3_task_3.py
+++ Lesson_3/lesson_3_task_3.py
@@ -5,5 +5,6 @@ to_address = Address("123456", "Москва", "Ленина", "10", "5")
 from_address = Address("654321", "Санкт-Петербург", "Невский пр.", "15", "10")
 mail = Mailing(to_address, from_address, 1000, "Заказное письмо")
 
-print(f"Отправление {mail.track} из {from_address.index}, {from_address.city}, {from_address.street}, {from_address.house} - {from_address.apartment} в {to_address.index}, {to_address.city}, {to_address.street}, {to_address.house} - {to_address.apartment}. Стоимость {mail.cost} рублей.")
-
+print(f"Отправление {mail.track} из {from_address.index}, {from_address.city}, {from_address.street}, {from_address.house}-{from_address.apartment} "
+      f"в {to_address.index}, {to_address.city}, {to_address.street}, {to_address.house}-{to_address.apartment}. "
+      f"Стоимость {mail.cost} рублей.")
